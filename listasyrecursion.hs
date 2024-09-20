d4002dd08cc1ec13cc23d47bf63c078edb425c4e
Santiago Morales <4santiago309@ciencias.unam.mx>
2024-09-20 17:49:10 -0600

    Add files via upload

---
+++ listasyrecursion.hs
@@ -0,0 +1,27 @@
+--------------- Listas y recursión ---------------
+
+longitud :: [a] -> Int
+longitud _ = undefined
+
+sumaLista :: Num a => [a] -> a
+sumaLista _ = undefined
+
+agregaElemento :: [a] -> a -> Bool -> [a]
+agregaElemento _ = undefined
+
+maximoLista :: (Num a, Ord a) => [a] -> a
+maximoLista _ = undefined
+
+indice :: [a] -> Int -> a
+indice _ = undefined
+
+--------------- Listas por comprehensión ---------------
+
+divisores :: Int -> [Int]
+divisores _ = undefined
+
+conjunto :: Eq a => [a] -> [a]
+conjunto _ = undefined
+
+numerosPares :: [Int] -> [Int]
+numerosPares _ = undefined
