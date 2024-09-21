5eb0159d3be7c9937d063b73e9cf520856dbae4d
morafrut <4santiago309@ciencias.unam.mx>
2024-09-20 19:14:43 -0600

    edite el archivo

--- listasyrecursion.hs
+++ listasyrecursion.hs
@@ -1,27 +1,33 @@
+
 --------------- Listas y recursión ---------------
 
 longitud :: [a] -> Int
-longitud _ = undefined
+longitud [] = 0
+longitud (x:xs) = 1 + longitud(xs) 
 
 sumaLista :: Num a => [a] -> a
-sumaLista _ = undefined
+sumaLista [] = 0              
+sumaLista (x:xs) = x + sumaLista xs  
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento _ = undefined
+agregaElemento xs elem True  = elem : xs  
+agregaElemento xs elem False = xs ++ [elem]  
 
 maximoLista :: (Num a, Ord a) => [a] -> a
-maximoLista _ = undefined
+maximoLista [x] = x 
+maximoLista (x:xs) = max x (maximoLista xs)
 
 indice :: [a] -> Int -> a
-indice _ = undefined
+indice (x:_) 0 = x
+indice (_:xs) n = indice xs (n-1)
 
 --------------- Listas por comprehensión ---------------
-
 divisores :: Int -> [Int]
-divisores _ = undefined
+divisores n = [x | x <- [1..n], n `div` x * x == n]
 
 conjunto :: Eq a => [a] -> [a]
-conjunto _ = undefined
+conjunto [] = []
+conjunto (x:xs) = x : conjunto [y | y <- xs, y /= x]
 
 numerosPares :: [Int] -> [Int]
-numerosPares _ = undefined
+numerosPares xs = [x | x <- xs, x `rem` 2 == 0]
