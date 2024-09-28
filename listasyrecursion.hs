f446d21e8ae3393c5674fb0d05c682c5926aae84
morafrut <4santiago309@ciencias.unam.mx>
2024-09-28 10:53:09 -0600

    actualize el archivo

--- listasyrecursion.hs
+++ listasyrecursion.hs
@@ -1,33 +1,46 @@
-
 --------------- Listas y recursión ---------------
 
 longitud :: [a] -> Int
 longitud [] = 0
-longitud (x:xs) = 1 + longitud(xs) 
+longitud (x:xs) = 1 + longitud xs
 
 sumaLista :: Num a => [a] -> a
 sumaLista [] = 0              
 sumaLista (x:xs) = x + sumaLista xs  
 
+-- 3. AgregaElemento en un solo caso:
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento xs elem True  = elem : xs  
-agregaElemento xs elem False = xs ++ [elem]  
+agregaElemento xs elem b = if b then elem : xs else xs ++ [elem]
 
+-- 4. maximoLista sin la función max:
 maximoLista :: (Num a, Ord a) => [a] -> a
-maximoLista [x] = x 
-maximoLista (x:xs) = max x (maximoLista xs)
+maximoLista [] = error "Lista vacía"
+maximoLista [x] = x
+maximoLista (x:xs) = maximoAux x xs
+  where
+    maximoAux y [] = y
+    maximoAux y (z:zs)
+      | z > y     = maximoAux z zs
+      | otherwise = maximoAux y zs
 
+-- 5. Índice con validación de rango:
 indice :: [a] -> Int -> a
-indice (x:_) 0 = x
-indice (_:xs) n = indice xs (n-1)
+indice xs n 
+  | n < 0 || n >= longitud xs = error "Índice fuera de rango"
+  | otherwise = indiceAux xs n
+  where
+    indiceAux (x:_) 0 = x
+    indiceAux (_:xs) m = indiceAux xs (m-1)
 
---------------- Listas por comprehensión ---------------
+--------------- Listas por comprensión ---------------
+-- 6 y 8. Uso de mod en lugar de rem:
 divisores :: Int -> [Int]
-divisores n = [x | x <- [1..n], n `div` x * x == n]
+divisores n = [x | x <- [1..n], n `mod` x == 0]
 
 conjunto :: Eq a => [a] -> [a]
 conjunto [] = []
 conjunto (x:xs) = x : conjunto [y | y <- xs, y /= x]
 
+-- 6 y 8. Uso de mod en lugar de rem:
 numerosPares :: [Int] -> [Int]
-numerosPares xs = [x | x <- xs, x `rem` 2 == 0]
+numerosPares xs = [x | x <- xs, x `mod` 2 == 0]
